277ed7afe3ada819ad10af248f86b3740cf07cc1
David Shaevel <david@davidshaevel.com>
2020-08-02 22:51:47 -0500

    Updated build agent configuration script

--- windows/configureBuildAgentVM.ps1
+++ windows/configureBuildAgentVM.ps1
@@ -5,22 +5,29 @@ Param(
     $azdo_pat
 )
 
-Set-ExecutionPolicy Bypass -Scope Process -Force; 
-#Install dotnet core, nodejs and npm with chocolatey
-iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'));
-choco feature enable -n allowGlobalConfirmation;
-choco install dotnetcore-sdk -y;
-choco install nodejs -y;
-choco install webdeploy -y;
+#Install Azure DevOps Build Agent
+$azdoAgentDirectory = Join-Path $env:SystemDrive 'agent';
+New-Item -Path $azdoAgentDirectory -ItemType Directory -force | Out-Null;
+Set-Location $azdoAgentDirectory
+Add-Type -AssemblyName System.IO.Compression.FileSystem ; [System.IO.Compression.ZipFile]::ExtractToDirectory("$HOME\Downloads\vsts-agent-win-x64-2.172.2.zip", "$PWD")
+config --unattended --url $azdo_server_url --auth pat --token $azdo_pat --pool $azdo_agent_pool --agent $azdo_agent_name --work "_work" --runAsService --runAsAutoLogon --windowsLogonAccount "NT AUTHORITY\NETWORK SERVICE" --noRestart
 
-$downloadDirectory = Join-Path $env:SystemDrive 'agent';
-New-Item -Path $downloadDirectory -ItemType Directory -force | Out-Null;
+#Install Chocolatey
+#Set-ExecutionPolicy Bypass -Scope Process -Force
+#Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-$clnt = new-object System.Net.WebClient;
-$url = "https://vstsagentpackage.azureedge.net/agent/2.129.1/vsts-agent-win-x64-2.129.1.zip";
-$file = Join-Path $downloadDirectory ([System.IO.Path]::GetFileName($url));
-$clnt.DownloadFile($url,$file);
+#Assign Packages to Install
+#$Packages = 'googlechrome',`
+#            'visualstudiocode',`
+#            'git',`
+#            'visualstudio2017community',`
+#            'visualstudio2017-workload-azure',`
+#            'visualstudio2017-workload-netweb'
 
-Expand-Archive -Path $file -DestinationPath $downloadDirectory;
+#Install Packages
+#ForEach ($PackageName in $Packages) {
+#    choco install $PackageName -y
+#}
 
-C:/agent/bin/Agent.Listener.exe configure --unattended --url $azdo_server_url --agent $azdo_agent_name --pool $azdo_agent_pool --auth PAT --token $azdo_pat --work "_work" --windowsLogonAccount "NT AUTHORITY\SYSTEM" --replace --runAsService --runAsAutoLogon;
+#Reboot
+#Restart-Computer
