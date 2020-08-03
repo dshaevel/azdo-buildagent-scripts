2c85efa84b6a056c783167f5cab92858987eb718
David Shaevel <david@davidshaevel.com>
2020-08-03 01:40:36 -0500

    Updated PowerShell script to download zip file to C:/temp

--- windows/configureBuildAgentVM.ps1
+++ windows/configureBuildAgentVM.ps1
@@ -6,17 +6,19 @@ Param(
 )
 
 #Install Azure DevOps Build Agent
+$downloadDirectory = Join-Path $env:SystemDrive 'temp';
 $azdoAgentDirectory = Join-Path $env:SystemDrive 'agent';
+New-Item -Path $downloadDirectory -ItemType Directory -force | Out-Null;
 New-Item -Path $azdoAgentDirectory -ItemType Directory -force | Out-Null;
 Set-Location $azdoAgentDirectory;
 
 $url = "https://vstsagentpackage.azureedge.net/agent/2.172.2/vsts-agent-win-x64-2.172.2.zip";
-$file = Join-Path $HOME\Downloads ([System.IO.Path]::GetFileName($url));
-(New-Object System.Net.WebClient).DownloadFile($url,$file);
+$file = Join-Path $downloadDirectory ([System.IO.Path]::GetFileName($url));
+(New-Object System.Net.WebClient).DownloadFile($url,$file)
 Expand-Archive -Path $file -DestinationPath $azdoAgentDirectory;
 
 .\config.cmd --unattended --url $azdo_server_url --auth pat --token $azdo_pat --pool $azdo_agent_pool --agent $azdo_agent_name --work "_work" --runAsService --runAsAutoLogon --windowsLogonAccount "NT AUTHORITY\NETWORK SERVICE" --noRestart
-
+ 
 #Install Chocolatey
 #Set-ExecutionPolicy Bypass -Scope Process -Force
 #Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
